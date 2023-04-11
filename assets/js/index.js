870c976e5563524f33d8d3b91923ab79751a1ad3
cyrilletual <cyrille.tual@gmail.com>
2023-04-11 15:33:04 +0200

    fini01

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,23 @@
 import feed from "./feed.js";
 
+function toggleSection (){
+    const section = document.querySelector("main section:nth-child(2)");
+    section.style.display !== "flex" ? section.style.display = "flex" : section.style.display = ""; // couche ajoutée par dessus css  
+}
+
+function toggleAside(){
+    const aside = document.querySelector("aside");
+    aside.style.display !== "none" ? aside.style.display ="none" : aside.style.display =""
+}
+
 window.addEventListener('DOMContentLoaded', () =>{
-    feed();
+    feed();  // charge le contenu de la page a partie du fichier de datas (data/index.js)
+
+    // listen sur les bts
+    document.getElementById ('btn-section')
+        .addEventListener('click', toggleSection);
+
+    document.getElementById ('btn-aside')
+        .addEventListener('click', toggleAside)    
 })
 
